bb6d11e42cb3caf8018a268de50bde521af227c3
David Wilson <wilsonD21@gmail.com>
2014-03-15 14:18:50 -0400

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ErasLoader::Application.config.secret_key_base = '2de2b090ced464eb2ecd9953f470d37bb19f4550d496b2cb535402c6450af2fc0aeea9c1166df1c885984604a70df57637115219f976eb5d4840ac69f1d9c1cd'
